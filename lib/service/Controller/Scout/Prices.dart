82ea74eef1dd0ca2aa8d0bb40705f45607386ce8
Kevin Kamto <kamtokevin@gmail.com>
2022-01-14 16:04:14 -0500

    working changes

--- lib/service/Controller/Scout/Prices.dart
+++ lib/service/Controller/Scout/Prices.dart
@@ -3,62 +3,59 @@
 import 'package:ax_dapp/service/Athlete.dart';
 import 'package:postgres/postgres.dart';
 
-class Prices {
-  late Athlete athlete;
-  late PostgreSQLConnection connection;
-  var databaseUrl;
-  var firstPrice, lastPrice;
+// This has to be rewritten into functional paradigm, it's not meant to be an object, instead an instantiable process (querying and returning results)
+late PostgreSQLConnection connection;
 
-  void getBookPrice() {
-    //
+final athletePrice = (String name) async {
+  print("QUERY: this came in: $name");
+
+  List<List<dynamic>> results = await connection.query(
+      "SELECT @name, first(price) as StartingPrice, last(price) as LatestPrice from nfl",
+      substitutionValues: {"name": name});
+
+  for (final row in results) {
+    var a = row[0];
+    var b = row[0];
+    return "results: $a \n $b";
   }
+};
 
-  Prices() {
-    confirmConnection();
-  }
+// This opens up a stream that
+void getMarketPrice(String $name) async {
+  // This is a stream maybe that returns a consistent value ?
+  // Stream per athlete page ?
+  connection = PostgreSQLConnection("139.99.74.201", 8812, "qdb",
+      username: "admin", password: "quest");
+  await connection
+      .open()
+      .then((value) => {print('Connection confirmed /n $value')});
+  print('[Postgres] Attempting Connection to Database');
+}
 
-  void connect() async {
-    connection = PostgreSQLConnection("139.99.74.201", 8812, "qdb",
-        username: "admin", password: "quest");
-    print('[Postgres] Attempting Connection to Database');
-  }
-
-  void confirmConnection() async {
-    connection = PostgreSQLConnection("139.99.74.201", 8812, "qdb",
-        username: "admin", password: "quest");
-    try {
-      await connection
-          .open()
-          .then((value) => {print('Connection confirmed /n $value')});
-    } catch (e) {
-      print('[Postgres] Error attempting to open connection : \n $e');
-    }
-  }
-
-  void getMarketPrice() {}
-
-  void query(String name) async {
-    print("QUERY: this came in: $name");
-    String query =
-        "SELECT $name, first(price) as StartingPrice, last(price) as LatestPrice from nfl;";
-
-    List<List<dynamic>> results = await connection.query(
-        "SELECT @name, first(price) as StartingPrice, last(price) as LatestPrice from nfl",
-        substitutionValues: {"name": name});
-
-    for (final row in results) {
-      var a = row[0];
-      var b = row[0];
-      print("results: $a \n $b");
-    }
-
-    // List<List<dynamic>> results = await connection.query("SELECT a, b FROM table WHERE a = @aValue", substitutionValues: {
-    //     "aValue" : 3
-    // });
-  }
-
-  void close() async {
-    // close the Postgresql connection
-    connection.close();
+void getLatestBookPrice(String name) async {
+  connection = PostgreSQLConnection("139.99.74.201", 8812, "qdb",
+      username: "admin", password: "quest");
+  await connection.open();
+  List<List<dynamic>> results = await connection.query(
+      "SELECT @name, first(price) as StartingPrice, last(price) as LatestPrice from nfl",
+      substitutionValues: {"name": name});
+  for (final row in results) {
+    var a = row[0];
+    var b = row[1];
+    print('Return Values $a, $b');
   }
 }
+
+//   void main() {
+//   final greeting = (String name) {
+//     return 'Hi $name';
+//   };
+
+//   saySomething(greeting);
+// }
+
+// void saySomething(Function(String) message) {
+//   print(message('Marcos'));
+// }
+
+// // Hi Marcos
