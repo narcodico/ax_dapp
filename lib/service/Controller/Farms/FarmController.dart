db810ca1676cdf52cc3fa7722e91d8b9b8f760b4
Kevin Kamto <kamtokevin@gmail.com>
2022-04-04 12:56:26 -0400

    chore: updated deposit + withdraw functions

--- lib/service/Controller/Farms/FarmController.dart
+++ lib/service/Controller/Farms/FarmController.dart
@@ -1,12 +1,35 @@
+import 'package:ax_dapp/contracts/StakingRewards.g.dart';
 import 'package:ax_dapp/service/Controller/Controller.dart';
+import 'package:web3dart/web3dart.dart';
+import '../../../contracts/StakingRewards.g.dart';
 import 'package:get/get.dart';
 import 'package:web3dart/credentials.dart';
 
 class FarmController extends GetxController {
   Controller controller = Get.find();
+  late StakingRewards _stakingRewards;
   var amount1 = 0.0.obs, amount2 = 0.0.obs;
 
   final EthereumAddress farmAddress = EthereumAddress.fromHex("hex");
+  FarmController();
 
-  void deposit() async {}
+  void deposit() async {
+
+    // _stakingRewards = StakingRewards(address: address, client: client);
+    final credentials = controller.credentials;
+    final amount = BigInt.from(amount1.value);
+    _stakingRewards.stake(amount, credentials: credentials);
+  }
+
+  void withdraw() async {
+    final credentials = controller.credentials;
+    final amount = BigInt.from(amount1.value);
+    _stakingRewards.withdraw(amount, credentials: credentials);
+  }
+
+  void updateAmount(double newAmount) {
+    amount1.value = newAmount;
+    print("Updated amount");
+    update();
+  }
 }
