331948a2ccf1a7893fa0b087ef1268ad5eae1009
Rolly <rolly@narcodic.com>
2022-07-29 09:49:43 +0300

    feat(tokens_repository): add `Token`s apis

--- packages/tokens_repository/lib/src/tokens_repository.dart
+++ packages/tokens_repository/lib/src/tokens_repository.dart
@@ -1,7 +1,25 @@
+import 'package:ethereum_api/ethereum_api.dart';
+
 /// {@template tokens_repository}
 /// Repository that manages the token domain.
 /// {@endtemplate}
 class TokensRepository {
   /// {@macro tokens_repository}
-  const TokensRepository();
+  TokensRepository({
+    required EthereumApiClient ethereumApiClient,
+  }) : _ethereumApiClient = ethereumApiClient;
+
+  final EthereumApiClient _ethereumApiClient;
+
+  /// Allows listening to changes to the current [Token]s.
+  Stream<List<Token>> get tokensChanges => _ethereumApiClient.tokensChanges;
+
+  /// Returns the current [Token]s synchronously. The returned [Token]s are
+  /// based on the current [EthereumChain].
+  List<Token> get tokens => _ethereumApiClient.tokens;
+
+  /// Allows switching the current [Token]s, which are set based on the current
+  /// [EthereumChain].
+  void switchTokens(EthereumChain chain) =>
+      _ethereumApiClient.switchTokens(chain);
 }
