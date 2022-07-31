c022c8223336d841b9d0b64fb7ac2d59dd0227bb
Rolly <rolly@narcodic.com>
2022-07-31 17:07:10 +0300

    feat(tokens_repository): expose current `apts` api

--- packages/tokens_repository/lib/src/tokens_repository.dart
+++ packages/tokens_repository/lib/src/tokens_repository.dart
@@ -21,12 +21,17 @@ class TokensRepository {
   /// Allows listening to changes to the current [Apt]s.
   Stream<List<Apt>> get aptsChanges => _ethereumApiClient.aptsChanges;
 
+  /// Returns the current [Apt]s synchronously. The returned [Apt]s are
+  /// based on the current [EthereumChain].
+  List<Apt> get apts => _ethereumApiClient.apts;
+
   /// Allows listening to changes to the [Apt]s (long and short) for the
   /// athlete identified by [athleteId].
   Stream<AptPair> aptPairChanges(int athleteId) =>
       _ethereumApiClient.aptPairChanges(athleteId);
 
   /// Returns the current [AptPair] for the given [athleteId] synchronously.
+  /// The returned [AptPair] is based on the current [EthereumChain].
   AptPair aptPair(int athleteId) => _ethereumApiClient.aptPair(athleteId);
 
   /// Allows switching the current [Token]s, which are set based on the current
