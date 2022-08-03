98b5dc0d724b97e6b7f794b95e77366510114d59
Rolly <rolly@narcodic.com>
2022-08-03 13:50:46 +0300

    feat(tokens_repository): expose `chainToken`

--- packages/tokens_repository/lib/src/tokens_repository.dart
+++ packages/tokens_repository/lib/src/tokens_repository.dart
@@ -36,23 +36,29 @@ class TokensRepository {
 
   /// Allows listening to changes to the [Token] associated with the current
   /// [EthereumChain].
-  Stream<Token> get chainTokenChanges => tokensChanges.map((tokens) {
-        final tokensChain = tokens.first.chain;
-        switch (tokensChain) {
-          case EthereumChain.none:
-          case EthereumChain.unsupported:
-            return Token.empty;
-          case EthereumChain.polygonMainnet:
-          case EthereumChain.polygonTestnet:
-            return tokens.axt;
-          case EthereumChain.sxMainnet:
-          case EthereumChain.sxTestnet:
-            return tokens.sxt;
-        }
-      });
+  Stream<Token> get chainTokenChanges => tokensChanges.map(_chainTokenMapper);
+
+  /// Returns the [Token] associated with the current [EthereumChain],
+  /// synchronously.
+  Token get chainToken => _chainTokenMapper(tokens);
 
   /// Allows switching the current [Token]s, which are set based on the current
   /// [EthereumChain].
   void switchTokens(EthereumChain chain) =>
       _ethereumApiClient.switchTokens(chain);
+
+  Token _chainTokenMapper(List<Token> tokens) {
+    final tokensChain = tokens.first.chain;
+    switch (tokensChain) {
+      case EthereumChain.none:
+      case EthereumChain.unsupported:
+        return Token.empty;
+      case EthereumChain.polygonMainnet:
+      case EthereumChain.polygonTestnet:
+        return tokens.axt;
+      case EthereumChain.sxMainnet:
+      case EthereumChain.sxTestnet:
+        return tokens.sxt;
+    }
+  }
 }
