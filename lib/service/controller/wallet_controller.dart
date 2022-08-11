b900f8bd6c947b3da02b402dca99767a17069627
Rolly <rolly@narcodic.com>
2022-08-11 05:40:05 +0300

    refactor: remove `buyAX` from `WalletController`

--- lib/service/controller/wallet_controller.dart
+++ lib/service/controller/wallet_controller.dart
@@ -2,7 +2,6 @@
 
 import 'package:ax_dapp/service/controller/controller.dart';
 import 'package:get/get.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class WalletController extends GetxController {
   late String axAddress;
@@ -12,13 +11,4 @@ class WalletController extends GetxController {
   RxString yourBalance = '-'.obs;
   RxDouble tokenPrice = 0.0.obs;
   Controller controller = Get.find();
-
-  void buyAX() {
-    // TODO(KevinKamto): Update this when we need sushiswap connection
-    // String axEth =
-    //     "https://app.sushi.com/swap?inputCurrency=0x5617604ba0a30e0ff1d2163ab94e50d8b6d0b0df&outputCurrency=0x7ceb23fd6bc0add59e62ac25578270cff1b9f619";
-    const axEthUniswap = 'https://app.uniswap.org/#/swap?chain=polygon';
-
-    launchUrl(Uri.parse(axEthUniswap));
-  }
 }
--- lib/service/dialog.dart
+++ lib/service/dialog.dart
@@ -1035,7 +1035,16 @@ Dialog yourAXDialog(BuildContext context) {
                       Colors.amber[600]!,
                     ),
                     child: TextButton(
-                      onPressed: walletController.buyAX,
+                      onPressed: () {
+                        // TODO(KevinKamto): Update this when we need sushiswap
+                        // connection
+                        // String axEth =
+                        //     "https://app.sushi.com/swap?inputCurrency=0x5617604ba0a30e0ff1d2163ab94e50d8b6d0b0df&outputCurrency=0x7ceb23fd6bc0add59e62ac25578270cff1b9f619";
+                        const axEthUniswap =
+                            'https://app.uniswap.org/#/swap?chain=polygon';
+
+                        launchUrl(Uri.parse(axEthUniswap));
+                      },
                       child: Text(
                         'Buy AX',
                         style: textStyle(Colors.black, 14, true),
