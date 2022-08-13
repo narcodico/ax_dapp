5f3cc75360e44109384eda0f7f070d1d01fc7df1
Rolly <rolly@narcodic.com>
2022-08-13 03:54:42 +0300

    feat(config_repository): expose `initializeAppConfig`

--- packages/config_repository/lib/src/config_repository.dart
+++ packages/config_repository/lib/src/config_repository.dart
@@ -1,7 +1,16 @@
+import 'package:ethereum_api/config_api.dart';
+
 /// {@template config_repository}
 /// Repository that manages configurations.
 /// {@endtemplate}
 class ConfigRepository {
   /// {@macro config_repository}
-  const ConfigRepository();
+  ConfigRepository({required ConfigApiClient configApiClient})
+      : _configApiClient = configApiClient;
+
+  final ConfigApiClient _configApiClient;
+
+  /// Creates and returns the initial [AppConfig] which is used to pass down
+  /// reactive dependencies.
+  AppConfig initializeAppConfig() => _configApiClient.initializeAppConfig();
 }
