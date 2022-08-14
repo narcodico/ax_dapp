156568fe701da020e408c3c6cbbbba058ae9967b
Rolly <rolly@narcodic.com>
2022-08-14 03:30:22 +0300

    refactor(ethereum_api): clean up `TokensApiClient`

--- packages/ethereum_api/lib/src/tokens/tokens_api_client.dart
+++ packages/ethereum_api/lib/src/tokens/tokens_api_client.dart
@@ -13,53 +13,25 @@ class TokensApiClient {
   final ValueStream<Web3Client> _reactiveWeb3Client;
   Web3Client get _web3Client => _reactiveWeb3Client.value;
 
-  final _tokensController = BehaviorSubject<List<Token>>();
+  final _tokensController = ReplaySubject<List<Token>>(maxSize: 2);
 
   /// Allows listening to changes to the current [Token]s.
   Stream<List<Token>> get tokensChanges => _tokensController.stream;
 
   /// Returns the current [Token]s synchronously. The returned [Token]s are
   /// based on the current [EthereumChain].
-  List<Token> get tokens => _tokensController.valueOrNull ?? const [];
+  List<Token> get currentTokens =>
+      _tokensController.values.lastOrNull ?? const [];
 
-  /// Allows listening to changes to the current [Apt]s.
-  Stream<List<Apt>> get aptsChanges => _tokensController.stream
-      .map((tokens) => tokens.whereType<Apt>().toList());
-
-  /// Returns the current [Apt]s synchronously. The returned [Apt]s are
-  /// based on the current [EthereumChain].
-  List<Apt> get apts => tokens.whereType<Apt>().toList();
-
-  /// Allows listening to changes to the [Apt]s (long and short) for the
-  /// athlete identified by [athleteId].
-  Stream<AptPair> aptPairChanges(int athleteId) =>
-      _tokensController.stream.map((tokens) => tokens.whereType<Apt>()).map(
-            (apts) => AptPair(
-              longApt: apts.singleWhere(
-                (apt) => apt.athleteId == athleteId && apt.type.isLong,
-                orElse: () => const Apt.empty(),
-              ),
-              shortApt: apts.singleWhere(
-                (apt) => apt.athleteId == athleteId && apt.type.isShort,
-                orElse: () => const Apt.empty(),
-              ),
-            ),
-          );
-
-  /// Returns the current [AptPair] for the given [athleteId] synchronously.
-  /// The returned [AptPair] is based on the current [EthereumChain].
-  AptPair aptPair(int athleteId) {
-    final apts = tokens.whereType<Apt>();
-    return AptPair(
-      longApt: apts.singleWhere(
-        (apt) => apt.athleteId == athleteId && apt.type.isLong,
-        orElse: () => const Apt.empty(),
-      ),
-      shortApt: apts.singleWhere(
-        (apt) => apt.athleteId == athleteId && apt.type.isShort,
-        orElse: () => const Apt.empty(),
-      ),
-    );
+  /// Returns the previous [Token]s synchronously. The returned [Token]s are
+  /// based on the previous [EthereumChain].
+  ///
+  /// Defaults to an empty list when there's no previous [EthereumChain],
+  /// meaning the wallet was not yet connected or it was just connected but the
+  /// chain wasn't yet changed.
+  List<Token> get previousTokens {
+    final values = _tokensController.values;
+    return values.length < 2 ? const [] : values.first;
   }
 
   /// Allows switching the current [Token]s, which are set based on the current
