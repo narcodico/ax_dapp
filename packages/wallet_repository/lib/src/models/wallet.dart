971529101c64d9b79ed5b178dde1ca34ff8950df
Rolly <rolly@narcodic.com>
2022-08-15 12:33:33 +0300

    feat(wallet_repository): expose `walletChanges`

---
+++ packages/wallet_repository/lib/src/models/wallet.dart
@@ -0,0 +1,35 @@
+import 'package:ethereum_api/wallet_api.dart';
+import 'package:shared/shared.dart';
+import 'package:wallet_repository/src/models/wallet_status.dart';
+
+/// {@template wallet}
+/// Holds wallet related data.
+/// {@endtemplate}
+class Wallet extends Equatable {
+  /// {@macro wallet}
+  const Wallet({
+    required this.status,
+    required this.address,
+    required this.chain,
+  });
+
+  /// Represents a disconnected [Wallet].
+  const Wallet.disconnected()
+      : this(
+          status: WalletStatus.disconnected,
+          address: kEmptyAddress,
+          chain: EthereumChain.none,
+        );
+
+  /// Wallet status.
+  final WalletStatus status;
+
+  /// Wallet address.
+  final String address;
+
+  /// Wallet chain.
+  final EthereumChain chain;
+
+  @override
+  List<Object?> get props => [status, address, chain];
+}
