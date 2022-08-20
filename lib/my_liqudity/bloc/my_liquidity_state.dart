3061f78a6b9c6c67c4310dcbc4fac3ca6f149a39
Rolly <rolly@narcodic.com>
2022-08-20 15:59:10 +0300

    feat(my_liquidity): refresh search on deps change

--- lib/my_liqudity/bloc/my_liquidity_state.dart
+++ lib/my_liqudity/bloc/my_liquidity_state.dart
@@ -2,38 +2,31 @@ part of 'my_liquidity_bloc.dart';
 
 class MyLiquidityState extends Equatable {
   const MyLiquidityState({
-    required this.cards,
-    required this.filteredCards,
-    required this.status,
+    this.status = BlocStatus.initial,
+    this.cards = const [],
+    this.filteredCards = const [],
+    this.searchTerm = '',
   });
 
-  factory MyLiquidityState.initial() {
-    return const MyLiquidityState(
-      cards: [],
-      filteredCards: [],
-      status: BlocStatus.initial,
-    );
-  }
-
+  final BlocStatus status;
   final List<LiquidityPositionInfo> cards;
   final List<LiquidityPositionInfo> filteredCards;
-  final BlocStatus status;
+  final String searchTerm;
 
   MyLiquidityState copyWith({
+    BlocStatus? status,
     List<LiquidityPositionInfo>? cards,
     List<LiquidityPositionInfo>? filteredCards,
-    BlocStatus? status,
+    String? searchTerm,
   }) {
     return MyLiquidityState(
+      status: status ?? this.status,
       cards: cards ?? this.cards,
       filteredCards: filteredCards ?? this.filteredCards,
-      status: status ?? this.status,
+      searchTerm: searchTerm ?? this.searchTerm,
     );
   }
 
   @override
-  List<Object> get props => [cards, status];
-
-  @override
-  String toString() => 'MyLiquidityState(cards: $cards, status: $status)';
+  List<Object> get props => [status, cards, filteredCards, searchTerm];
 }
