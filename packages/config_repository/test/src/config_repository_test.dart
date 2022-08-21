203f443e2e4cad65d1aa430379f7add7ff5b57a5
Rolly <rolly@narcodic.com>
2022-08-21 12:40:32 +0300

    test(config_repository): fix tests

--- packages/config_repository/test/src/config_repository_test.dart
+++ packages/config_repository/test/src/config_repository_test.dart
@@ -1,35 +1,23 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:config_repository/config_repository.dart';
 import 'package:ethereum_api/config_api.dart';
-import 'package:ethereum_api/tokens_api.dart';
-import 'package:ethereum_api/wallet_api.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
 class MockConfigApiClient extends Mock implements ConfigApiClient {}
 
-class MockWalletApiClient extends Mock implements WalletApiClient {}
-
-class MockTokensApiClient extends Mock implements TokensApiClient {}
-
 void main() {
   late ConfigApiClient configApiClient;
-  late WalletApiClient walletApiClient;
-  late TokensApiClient tokensApiClient;
 
   group('ConfigRepository', () {
     setUp(() {
       configApiClient = MockConfigApiClient();
-      walletApiClient = MockWalletApiClient();
-      tokensApiClient = MockTokensApiClient();
     });
 
     test('can be instantiated', () {
       expect(
         ConfigRepository(
           configApiClient: configApiClient,
-          walletApiClient: walletApiClient,
-          tokensApiClient: tokensApiClient,
         ),
         isNotNull,
       );
