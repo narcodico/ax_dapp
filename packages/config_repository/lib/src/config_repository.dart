3426fcd9655e163047e6d1306269de54746661ec
Rolly <rolly@narcodic.com>
2022-08-21 03:07:19 +0300

    feat(config_repository): expose `appDataChanges`

--- packages/config_repository/lib/src/config_repository.dart
+++ packages/config_repository/lib/src/config_repository.dart
@@ -1,16 +1,39 @@
+import 'package:config_repository/src/models/models.dart';
 import 'package:ethereum_api/config_api.dart';
 import 'package:ethereum_api/lsp_api.dart';
+import 'package:ethereum_api/tokens_api.dart';
 import 'package:ethereum_api/wallet_api.dart';
+import 'package:shared/shared.dart';
 
 /// {@template config_repository}
 /// Repository that manages configurations.
 /// {@endtemplate}
 class ConfigRepository {
   /// {@macro config_repository}
-  ConfigRepository({required ConfigApiClient configApiClient})
-      : _configApiClient = configApiClient;
+  ConfigRepository({
+    required ConfigApiClient configApiClient,
+    required WalletApiClient walletApiClient,
+    required TokensApiClient tokensApiClient,
+  })  : _configApiClient = configApiClient,
+        _walletApiClient = walletApiClient,
+        _tokensApiClient = tokensApiClient;
 
   final ConfigApiClient _configApiClient;
+  final WalletApiClient _walletApiClient;
+  final TokensApiClient _tokensApiClient;
+
+  /// Allows listening to when all app data has finished changing as a result
+  /// of chain switching.
+  ///
+  /// This is particularly useful when we need to make sure the chain, tokens
+  /// and dependencies finished changing before refetching information.
+  Stream<AppData> get appDataChanges =>
+      ZipStream.zip3<EthereumChain, List<Token>, void, AppData>(
+        _walletApiClient.chainChanges,
+        _tokensApiClient.tokensChanges,
+        _configApiClient.dependenciesChanges,
+        (chain, tokens, _) => AppData(chain: chain, tokens: tokens),
+      );
 
   /// Allows listening to when dependencies change. Used to refetch data that
   /// is based on reactive dependencies.
